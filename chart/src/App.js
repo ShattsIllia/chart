66ea2e490bcb3745c0205a7fee13a5bd31ba646d
Tony Corsair <corsair@MacBook-Pro-Tony.local>
2021-09-17 12:39:19 +0300

    connect to socket io

--- chart/src/App.js
+++ chart/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import {io} from "socket.io-client";
 
 function App() {
+    const socket = io("ws://localhost:3003", {
+        transports: ["websocket", "polling"] // use WebSocket first, if available
+    });
+    socket.on("data", (...data) => {
+        // revert to classic upgrade
+        console.log(data)
+    });
   return (
     <div className="App">
-
+            Hello World!
       dsadasdasdad......
     </div>
   );
