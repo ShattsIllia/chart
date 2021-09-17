9909f1f06b9a4f06d4872d28482368532ee857a6
AlVel <welmozhko88@gmail.com>
2021-09-17 15:22:28 +0300

    added getTime function

--- chart/src/App.js
+++ chart/src/App.js
@@ -1,13 +1,23 @@
 import "./App.css";
-// import io from "socket.io-client";
 import { useState } from "react";
 import { socket } from "./socket";
 
 function App() {
   const [data, setData] = useState({
     value: 0,
-    timestamp: 1631878046524,
-  });
+    timestamp: 1631878046524
+  })
+
+  const getTime = (timestamp) => {
+      const date = new Date(timestamp)
+      const hours = date.getHours()
+      const minutes = date.getMinutes()
+      const seconds = date.getSeconds()
+
+      return `${hours}:${minutes<10?0: ''}${minutes}:${seconds<10?0:''}${seconds}`
+  }
+
+
 
   socket.on("data", (...data) => {
     // revert to classic upgrade
@@ -18,9 +28,11 @@ function App() {
 
   return (
     <div className="App">
-      <div>{`value: ${data.value}`}</div>
+        <div>{`value: ${data.value}`}</div>
+        
+        {getTime(data.timestamp)}
 
-      {`timeStamp: ${Date(data.timestamp)}`}
+     
     </div>
   );
 }
