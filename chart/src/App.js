f298daa73c2f42c1b67a58c63dc2ff42872026d2
Tony Corsair <corsair@MacBook-Pro-Tony.local>
2021-09-17 14:51:53 +0300

    add prettier, format files

--- chart/src/App.js
+++ chart/src/App.js
@@ -1,33 +1,26 @@
-import './App.css';
+import "./App.css";
 // import io from "socket.io-client";
-import { useState } from 'react';
-import { socket } from './socket';
+import { useState } from "react";
+import { socket } from "./socket";
 
 function App() {
-
   const [data, setData] = useState({
     value: 0,
-    timestamp: 1631878046524
-  })
-
-
-
-
-
-  
-  socket.on("data", (...data) => {
-      // revert to classic upgrade
-      setData(data[0]);
+    timestamp: 1631878046524,
   });
 
- console.log();
+  socket.on("data", (...data) => {
+    // revert to classic upgrade
+    setData(data[0]);
+  });
+
+  console.log();
 
   return (
     <div className="App">
-        <div>{`value: ${data.value}`}</div>
-        
-        {`timeStamp: ${Date(data.timestamp)}`}
+      <div>{`value: ${data.value}`}</div>
 
+      {`timeStamp: ${Date(data.timestamp)}`}
     </div>
   );
 }
--- chart/src/socket.js
+++ chart/src/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-export   const socket = io("ws://localhost:3001", {
+export const socket = io("ws://localhost:3001", {
     transports: ["websocket", "polling"] // use WebSocket first, if available
 });
