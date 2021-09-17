b6fb5e8028f81272106c9b80df267308954e20eb
Tony Corsair <corsair@MacBook-Pro-Tony.local>
2021-09-17 15:07:24 +0300

    format code

--- chart/src/App.js
+++ chart/src/App.js
@@ -1,34 +1,24 @@
-import './App.css';
+import "./App.css";
 import io from "socket.io-client";
-import React, {useEffect, useState} from 'react';
+import React, { useState } from "react";
 
-import { LineChart, Line } from 'recharts';
+// import { LineChart, Line } from "recharts";
 
 function App() {
-   const [number, setNumber] = useState(0);
+  const [number, setNumber] = useState(0);
 
-   const numbersArr = [];
+  const numbersArr = [];
 
- 
-    const socket = io("ws://localhost:3001", {
-        transports: ["websocket", "polling"] // use WebSocket first, if available
-    });
+  const socket = io("ws://localhost:3001", {
+    transports: ["websocket", "polling"], // use WebSocket first, if available
+  });
 
-    socket.on("data", (data) => {
-        // revert to classic upgrade
-       
-        numbersArr.push(data.value)
-        console.log(numbersArr)
-      
-    });
+  socket.on("data", (data) => {
+    numbersArr.push(data.value);
+    console.log(numbersArr);
+  });
 
-    
-  
-  return (
-    <div className="App">
-           
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
